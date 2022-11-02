b78e2dec5ab9d44147cba15b15b5b0210dbee778
rayanmazari90 <rayanmazari90@gmail.com>
2022-11-02 20:39:12 +0100

    RAYANE MAZARI

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+from dbm import dumb
 from flask import Flask, render_template, request, redirect
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
@@ -43,12 +44,36 @@ def delete(id):
         return redirect('/')
     except:
         return 'There was a problem deleting that task'
-    
-    
 
-@app.route('/update')
-def update():
-    return render_template('update.html')
+@app.route('/update/<int:id>')
+
+def get_id(id):
+    return render_template('update.html', task= id)
+
+@app.route('/updtating_form/<int:id>', methods=['POST', 'GET'])
+def update(id):
+    task_to_delete = Todo.query.get_or_404(id)
+    try:
+        db.session.delete(task_to_delete)
+        db.session.commit()
+    except:
+        return 'There was a problem deleting that task'
+
+    if request.method == 'POST':
+        task_content = request.form['content']
+        new_task = Todo(content=task_content)
+        new_task.id=id
+        try:
+            db.session.add(new_task)
+            db.session.commit()
+            return redirect('/')
+        except:
+            return 'There was an issue adding your task'
+        return render_template('index.html')
+    else:
+        tasks = Todo.query.order_by(Todo.date_created).all()
+        return render_template('update.html', tasks=tasks)
+    return render_template('update.html', task= id)
 
 if __name__ == '__main__':
     app.run(debug=True)
