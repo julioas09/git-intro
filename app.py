239c8d8d455f42b5aed837c5658e4e8cd6371a46
yalsaffar <95584811+yalsaffar@users.noreply.github.com>
2022-11-04 00:01:47 +0100

    Update app.py

--- app.py
+++ app.py
@@ -48,27 +48,18 @@ def delete(id):
 
 @app.route('/updtating_form/<int:id>', methods=['POST', 'GET'])
 def update(id):
-    task_to_delete = Todo.query.get_or_404(id)
-    try:
-        db.session.delete(task_to_delete)
-        db.session.commit()
-    except:
-        return 'Something Went Wrong'
+
+    task = Todo.query.get_or_404(id)
 
     if request.method == 'POST':
-        task_content = request.form['content']
-        new_task = Todo(content=task_content)
-        new_task.id=id
+        task.content = request.form['contentNew']
         try:
-            db.session.add(new_task)
             db.session.commit()
             return redirect('/')
         except:
-            return 'Something Went Wrong'
-        return render_template('index.html')
+            return 'There was a problem'
     else:
-        tasks = Todo.query.order_by(Todo.date_created).all()
-        return render_template('update.html', tasks=tasks)
-    return render_template('update.html', task= id)
+        return render_template('update.html', task=task)
+
 if __name__ == '__main__':
     app.run(debug=True)
